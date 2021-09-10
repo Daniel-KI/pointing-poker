e9a5d6628e7ce1f69e21765ea7876fc953dae91e
kagerka <kagerka@gmail.com>
2021-09-11 00:52:18 +0300

    feat: add props from parent component

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './Toggle.scss';
-import '../../utils/colors.scss';
 
-const Toggle: React.FC = () => {
+/*
+  По умолчанию через пропсы приходит false для toggle. При изменении меняется на true.
+  В родительском компоненте это выглядит следующим образом:
+  const [checked, toggleChecked] = useState<boolean>(false);
+  <Toggle checked={checked} onChange={toggleChecked} />
+*/
+
+interface ToggleProps {
+  checked: boolean;
+  onChange: (value: boolean) => void;
+}
+
+const Toggle: React.FC<ToggleProps> = ({ checked, onChange }) => {
+  const toggleValue = () => {
+    onChange(!checked);
+  };
   return (
     <div>
       <label htmlFor='toggle' className='toggle'>
-        <input id='toggle' type='checkbox' />
+        <input id='toggle' type='checkbox' checked={checked} onChange={toggleValue} />
         <span className='toggle__slider toggle__circle' />
       </label>
     </div>
