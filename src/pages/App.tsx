9dd71e6eaac6b479bfa52c8455cb8b0a6c128944
Daniil Korshov <dankorshov@gmail.com>
2021-09-14 16:16:42 +0300

    fix: remove unnecessary elements

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from 'react';
-import Button from '../components/Button/Button';
 
 import './App.scss';
 
@@ -7,10 +6,6 @@ function App(): ReactElement {
   return (
     <div className='App'>
       <h1>Pointing poker</h1>
-      <Button onClick={() => console.log('qwe')} color='dark' size='small'>
-        qwe
-        <div>qwe</div>
-      </Button>
     </div>
   );
 }
