f57b9bb59914d243708bb3d15a27f9962ac40b76
Daniil Korshov <dankorshov@gmail.com>
2021-09-14 12:13:40 +0300

    feat: add adaptive layout

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import './Main.scss';
+import { ReactComponent as TeamImg } from '../../assets/team.svg';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 
 const Main: React.FC = () => {
   return (
-    <div className='page main'>
+    <div className='main'>
       <Header />
-      <div className='main__container' />
+      <div className='main__wrapper'>
+        <div className='main__container'>
+          <h2 className='main__title'>Plan your project</h2>
+          <p className='main__description'>
+            Planning poker is a consensus-based, gamified technique for estimating, mostly used for timeboxing in Agile
+            principles.
+          </p>
+          <div className='main__interactions'>
+            <Button color='primary' className='main__btn'>
+              Create
+            </Button>
+            <Button color='success' className='main__btn'>
+              Join
+            </Button>
+          </div>
+          <div className='main__image-container'>
+            <TeamImg className='main__image' />
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
