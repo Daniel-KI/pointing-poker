77ce96cb138c1b085dfca85f076feb88fb9f2250
Daniil Korshov <dankorshov@gmail.com>
2021-09-15 18:15:37 +0300

    fix: fix header component styles, files structure and imports

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,24 +3,21 @@ import './Header.scss';
 import { IoChatboxEllipses } from 'react-icons/io5';
 import classNames from 'classnames';
 import { ReactComponent as AppLogo } from '../../assets/app-logo.svg';
-
-interface HeaderProps {
-  isAuthorized?: boolean;
-}
+import { HeaderProps } from './models';
 
 const Header: React.FC<HeaderProps> = ({ isAuthorized }) => {
   const classes = classNames({
     header: true,
-    header_centered: !isAuthorized,
+    'header--centered': !isAuthorized,
   });
 
   return (
     <header className={classes}>
       <div className='header__container'>
-        <div className='header__logo'>
-          <a href='/' className='header__logo-link'>
-            <AppLogo className='header__logo-img' />
-            <h1 className='header__logo-name'>Pointing poker</h1>
+        <div className='logo'>
+          <a href='/' className='logo__link'>
+            <AppLogo className='logo__img' />
+            <h1 className='logo__name'>Pointing poker</h1>
           </a>
         </div>
         {isAuthorized ? <IoChatboxEllipses className='header__chat' /> : <></>}
