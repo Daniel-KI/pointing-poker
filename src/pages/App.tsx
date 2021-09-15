b6c8c255ea72fe296faeb92fffa23c7c9d8a8606
Daniil Korshov <dankorshov@gmail.com>
2021-09-15 14:22:19 +0300

    fix: remove unnecessary elements

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from 'react';
-import TextInput from '../components/TextInput/TextInput';
 
 import './App.scss';
 
@@ -7,16 +6,6 @@ function App(): ReactElement {
   return (
     <div className='App'>
       <h1>Pointing poker</h1>
-      <TextInput disabled />
-      <TextInput disabled />
-      <TextInput bordered />
-      <TextInput color='primary' size='small' name='qwe' />
-      <TextInput bordered color='light' />
-      <TextInput bordered color='warning' />
-      <TextInput bordered color='danger' />
-      <TextInput bordered color='success' />
-      <TextInput bordered color='dark' />
-      <TextInput bordered color='light' />
     </div>
   );
 }
