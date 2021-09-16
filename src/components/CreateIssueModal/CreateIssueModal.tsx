2805129d5b3025b5230d87657ec94a5e40be684e
kagerka <kagerka@gmail.com>
2021-09-16 20:26:48 +0300

    fix: add onConfirm and onDecline props, ModalBox

--- src/components/CreateIssueModal/CreateIssueModal.tsx
+++ src/components/CreateIssueModal/CreateIssueModal.tsx
@@ -4,42 +4,59 @@ import TextInput from '../TextInput/TextInput';
 import { CreateIssueModalProps } from './models';
 import Button from '../Button/Button';
 import Dropdown from '../Dropdown/Dropdown';
+import ModalBox from '../ModalBox/ModalBox';
 
-const CreateIssueModal: React.FC<CreateIssueModalProps> = ({ setActive }) => {
+// Родительский компонент:
+// const [isActive, setActive] = useState(false);
+// const modalActive = () => {
+//   setActive(true);
+// };
+// const onConfirm = () => {
+//   setActive(false);
+//   console.log('confirm');
+// };
+// const onDecline = () => {
+//   setActive(false);
+//   console.log('decline');
+// };
+
+// Появление модального окна при нажатии на кнопку
+//   <Button onClick={modalActive}>Modal</Button>
+//   <CreateIssueModal isActive={isActive} setActive={setActive} onDecline={onDecline} onConfirm={onConfirm} />
+
+const CreateIssueModal: React.FC<CreateIssueModalProps> = ({ setActive, isActive, onConfirm, onDecline }) => {
   const [priority, setPriority] = useState<string | undefined>(undefined);
 
-  const modalActive = () => {
-    setActive(false);
-  };
-
   return (
-    <div className='create-issue'>
-      <h2 className='create-issue__title'>Create issue</h2>
-      <form className='create-issue__form'>
-        <div className='create-issue__wrapper'>
-          <div className='create-issue__text-inputs'>
-            <TextInput placeholder='Title' size='large' color='light' bordered />
-            <TextInput placeholder='Link' size='large' color='light' bordered />
+    <ModalBox active={isActive} setActive={setActive}>
+      <div className='create-issue'>
+        <h2 className='create-issue__title'>Create issue</h2>
+        <form className='create-issue__form'>
+          <div className='create-issue__wrapper'>
+            <div className='create-issue__text-inputs'>
+              <TextInput placeholder='Title' size='large' color='light' bordered />
+              <TextInput placeholder='Link' size='large' color='light' bordered />
+            </div>
+            <div className='create-issue__dropdown'>
+              <Dropdown
+                options={['low', 'medium', 'hight']}
+                selected={priority}
+                setSelected={setPriority}
+                className='create-issue__dropdown_input'
+              />
+            </div>
           </div>
-          <div className='create-issue__dropdown'>
-            <Dropdown
-              options={['low', 'medium', 'hight']}
-              selected={priority}
-              setSelected={setPriority}
-              className='create-issue__dropdown_input'
-            />
+          <div className='create-issue__buttons'>
+            <Button color='success' size='large' className='create-issue__buttons_button' onClick={onConfirm}>
+              Confirm
+            </Button>
+            <Button color='danger' size='large' className='create-issue__buttons_button' onClick={onDecline}>
+              Cancel
+            </Button>
           </div>
-        </div>
-        <div className='create-issue__buttons'>
-          <Button color='success' size='large' className='create-issue__buttons_button'>
-            Confirm
-          </Button>
-          <Button color='danger' size='large' className='create-issue__buttons_button' onClick={modalActive}>
-            Cancel
-          </Button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </ModalBox>
   );
 };
 
