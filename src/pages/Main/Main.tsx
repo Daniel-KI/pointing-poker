fa3c4ef89b059f2147c56f3287b36c7f1896c7b3
Daniil Korshov <dankorshov@gmail.com>
2021-09-16 12:11:06 +0300

    feat: add input and add styles

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,8 +4,15 @@ import { ReactComponent as TeamImg } from '../../assets/team.svg';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
+import TextInput from '../../components/TextInput/TextInput';
 
 const Main: React.FC = () => {
+  const onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const onCreateBtnClick = () => {};
+
   return (
     <div className='main'>
       <Header />
@@ -16,14 +23,22 @@ const Main: React.FC = () => {
             Planning poker is a consensus-based, gamified technique for estimating, mostly used for timeboxing in Agile
             principles.
           </p>
-          <div className='main__interactions'>
-            <Button color='primary' className='main__btn'>
+          <form className='main__connection' onSubmit={onFormSubmit}>
+            <TextInput
+              bordered
+              color='light'
+              placeholder='Enter lobby url here...'
+              name='url'
+              className='main__url-input'
+              required
+            />
+            <Button color='primary' className='main__btn main__btn--create' onClick={onCreateBtnClick}>
               Create
             </Button>
-            <Button color='success' className='main__btn'>
+            <Button color='success' className='main__btn main__btn--join' submit>
               Join
             </Button>
-          </div>
+          </form>
           <div className='main__image-container'>
             <TeamImg className='main__image' />
           </div>
