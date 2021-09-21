05dee52e67cb3c0253e39203ec1b8043d82172cd
Daniil Korshov <dankorshov@gmail.com>
2021-09-21 16:02:14 +0300

    fix: fix layout, remove unnecessary props, add hover effect

--- src/components/SPCard/SPCard.tsx
+++ src/components/SPCard/SPCard.tsx
@@ -11,48 +11,57 @@ import DataControlPanel from '../DataControlPanel/DataControlPanel';
 // <SPCard editAction={editAction} deleteAction={deleteAction} />
 
 // Чтобы была стандартная карта с цифрами:
-// <SPCard scoreType='SP' cardScore='15' cardType='default' editAction={editAction} deleteAction={deleteAction} />
+// <SPCard scoreType='SP' cardScore='15' editAction={editAction} deleteAction={deleteAction} />
 
-const SPCard: React.FC<SPCardProps> = ({ scoreType, cardScore, cardType, editAction, deleteAction, className, id }) => {
-  const classes = classNames(className);
+// const editAction = () => {
+//   alert('edit');
+// };
 
-  const onMouseClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const card = event.currentTarget as HTMLElement;
-    if (card.dataset.card === 'card') {
-      card.classList.toggle('sp-card--active');
-    }
-  };
+// const deleteAction = () => {
+//   alert('del');
+// };
+
+const SPCard: React.FC<SPCardProps> = ({ scoreType, cardScore, editAction, deleteAction, className, id }) => {
+  const classes = classNames(
+    {
+      'sp-card': true,
+    },
+    className,
+  );
 
   return (
-    <div className={`${classes} sp-card`} id={id} onClick={onMouseClick} data-card='card' role='presentation'>
-      <div className='sp-card__card-edit'>
-        <DataControlPanel deleteAction={deleteAction} className='sp-card__card-edit_edit-btn' />
-        <DataControlPanel editAction={editAction} className='sp-card__card-edit_edit-btn' />
-      </div>
-      <div className='sp-card__inner-border'>
-        {cardType === 'default' ? (
-          <div className='sp-card__content'>
-            <div className='sp-card__type-top'>
-              <div>{scoreType}</div>
-            </div>
-            <div className='sp-card__card-score'>{cardScore}</div>
-            <div className='sp-card__type-bottom'>{scoreType}</div>
-          </div>
-        ) : null}
-        {cardType === undefined ? (
-          <div className='sp-card__content'>
-            <div className='sp-card__type-top'>
-              <div>Unknown</div>
-            </div>
-            <div className='sp-card__circle sp-card__circle--question'>
+    <div className={classes} id={id} data-card='card'>
+      <div className='sp-card__container'>
+        <div className='sp-card__controls'>
+          <DataControlPanel
+            deleteAction={deleteAction}
+            editAction={editAction}
+            className='sp-card__card-edit_edit-btn'
+          />
+        </div>
+        <p className='sp-card__top-text'>{scoreType}</p>
+        <div className='sp-card__body'>
+          {cardScore ? (
+            <p className='sp-card__score'>{cardScore}</p>
+          ) : (
+            <div className='sp-card__circle'>
               <IoHelp className='sp-card__question-icon' />
             </div>
-            <div className='sp-card__type-bottom'>Unknown</div>
-          </div>
-        ) : null}
+          )}
+        </div>
+        <div className='sp-card__bottom-text'>{scoreType}</div>
       </div>
     </div>
   );
+};
+
+SPCard.defaultProps = {
+  className: undefined,
+  id: undefined,
+  editAction: undefined,
+  deleteAction: undefined,
+  scoreType: 'unknown',
+  cardScore: undefined,
 };
 
 export default SPCard;
--- src/components/SPCard/models.ts
+++ src/components/SPCard/models.ts
@@ -1,9 +1,8 @@
 export interface SPCardProps {
-  className?: string;
-  id?: string;
   editAction?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   deleteAction?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   scoreType?: string | undefined;
   cardScore?: string;
-  cardType?: string | undefined;
+  className?: string;
+  id?: string;
 }
