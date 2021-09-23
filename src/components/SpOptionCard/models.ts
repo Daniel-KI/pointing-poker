c5a72a31426c76a3652d3a017b2b36734b01690c
Daniil Korshov <dankorshov@gmail.com>
2021-09-23 13:21:29 +0300

    fix: rename component and change layout

--- src/components/SpOptionCard/models.ts
+++ src/components/SpOptionCard/models.ts
@@ -1,8 +1,8 @@
-export interface SpCardProps {
+export interface SpOptionCardProps {
+  units?: string | undefined;
+  score?: string;
   editAction?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   deleteAction?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  scoreType?: string | undefined;
-  cardScore?: string;
   className?: string;
   id?: string;
 }
