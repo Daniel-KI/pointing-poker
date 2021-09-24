2003de4a2f049bb4ca9694791caabf84ad427e94
Daniil Korshov <dankorshov@gmail.com>
2021-09-24 11:38:06 +0300

    feat: add selected styles

--- src/components/SpCardBack/SpCardBack.tsx
+++ src/components/SpCardBack/SpCardBack.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './SpCardBack.scss';
+import { IoChevronDown } from 'react-icons/io5';
 import classNames from 'classnames';
 import { SpCardBackProps } from './models';
 import spCardBacks from '../../constants/spCardBacks';
 
-const SpCardBack: React.FC<SpCardBackProps> = ({ type, onClick, className, id }) => {
+const SpCardBack: React.FC<SpCardBackProps> = ({ type, onClick, isSelected, className, id }) => {
   const classes = classNames(
     {
       'sp-card-back': true,
+      'sp-card-back--selected': isSelected,
     },
     className,
   );
@@ -15,6 +17,11 @@ const SpCardBack: React.FC<SpCardBackProps> = ({ type, onClick, className, id })
 
   return (
     <button className={classes} id={id} onClick={onClick} type='button'>
+      <div className='sp-card-back__mark'>
+        <div className='sp-card-back__circle'>
+          <IoChevronDown className='sp-card-back__mark-icon' />
+        </div>
+      </div>
       <div className='sp-card-back__container'>
         <img src={cardBack.image} alt={cardBack.type} className='sp-card-back__image' />
       </div>
@@ -24,6 +31,7 @@ const SpCardBack: React.FC<SpCardBackProps> = ({ type, onClick, className, id })
 
 SpCardBack.defaultProps = {
   onClick: undefined,
+  isSelected: false,
   className: undefined,
   id: undefined,
 };
--- src/components/SpCardBack/models.ts
+++ src/components/SpCardBack/models.ts
@@ -3,6 +3,7 @@ import CardBack from '../../types/SpCardBackType';
 export interface SpCardBackProps {
   type: CardBack;
   onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  isSelected?: boolean;
   className?: string;
   id?: string;
 }
