ae815ce51f1aedc5dcfacdf9d7bef91c08aec8d8
kagerka <kagerka@gmail.com>
2021-09-25 23:54:02 +0300

    fix: add download stats button

--- src/pages/GameResult/GameResult.tsx
+++ src/pages/GameResult/GameResult.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './GameResult.scss';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import { GameResultListProps, GameResultProps, StatisticsCardsProps } from './models';
 import IssueCard from '../../components/IssueCard/IssueCard';
 import SpCardFront from '../../components/SpCardFront/SpCardFront';
+import Button from '../../components/Button/Button';
 
 const GameResult: React.FC<GameResultProps> = ({ lobbyTitle, gameResult }) => {
   return (
@@ -12,7 +13,11 @@ const GameResult: React.FC<GameResultProps> = ({ lobbyTitle, gameResult }) => {
       <Header isAuthorized />
       <div className='game-result__wrapper'>
         <h2 className='game-result__title'>{lobbyTitle}</h2>
-
+        <div className='game-result__download-btn-wrapper'>
+          <Button color='success' size='large' className='game-result__download-btn'>
+            Download statistics
+          </Button>
+        </div>
         <div className='game-result__statistics'>
           {gameResult?.map((issue: GameResultListProps, index: number) => (
             <div className='game-result__statistics_statistic-wrapper'>
