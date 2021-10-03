f8da522d97609af66c510626c37c4e19da875529
Daniil Korshov <dankorshov@gmail.com>
2021-10-03 11:20:40 +0300

    fix: fix validation and ConnectModal

--- src/utils/validation/validateLobbyId.ts
+++ src/utils/validation/validateLobbyId.ts
@@ -2,9 +2,9 @@ import emptyStringValidation from './emptyStringValidation';
 
 const validateLobbyId = (url: string): string => {
   const isNotEmpty = emptyStringValidation(url);
-  const isOnlyValidCharacters = url.match('^[a-zA-Z0-9]+$');
+  const isOnlyValidCharacters = url.match(/^[a-zA-Z0-9\-_]+$/);
   if (!isNotEmpty) return 'This field cannot be empty';
-  if (!isOnlyValidCharacters) return 'ID can consist only numbers and latin letters';
+  if (!isOnlyValidCharacters) return 'ID can consist only numbers, latin letters, dash and underscore';
   return '';
 };
 
