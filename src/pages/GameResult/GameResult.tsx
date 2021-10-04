e255df0aa26f74bdcbc36accd0877a7e8436b683
Daniil Korshov <dankorshov@gmail.com>
2021-10-04 17:23:42 +0300

    feat: add download function, refactor layout and styles, add redux, fix models

--- src/pages/GameResult/GameResult.tsx
+++ src/pages/GameResult/GameResult.tsx
@@ -3,52 +3,66 @@ import './GameResult.scss';
 import { useSelector } from 'react-redux';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
-import { GameResultListProps, GameResultProps, StatisticsCardsProps } from './models';
 import IssueCard from '../../components/IssueCard/IssueCard';
 import SpCardFront from '../../components/SpCardFront/SpCardFront';
 import Button from '../../components/Button/Button';
-import Chat from '../../components/Chat/Chat';
 import { IState } from '../../redux/models';
+import downloadJsonFile from '../../utils/downloadJsonFile';
 
-const GameResult: React.FC<GameResultProps> = ({ lobbyTitle, gameResult }) => {
-  const messages = useSelector((state: IState) => state.messages);
+const GameResult: React.FC = () => {
+  const lobbyTitle = useSelector((state: IState) => state.room.name);
+  const gameResult = useSelector((state: IState) => state.gameResults);
+  const scoreType = useSelector((state: IState) => state.settings.scoreType);
+
+  const downloadBtnOnClick = () => {
+    const jsonData = JSON.stringify(gameResult);
+    downloadJsonFile(jsonData, `statistics`);
+  };
 
   return (
     <div className='game-result'>
       <Header isAuthorized />
       <div className='game-result__wrapper'>
-        <div className='game-result__results'>
-          <h2 className='game-result__title'>{lobbyTitle}</h2>
-          <div className='game-result__download-btn-wrapper'>
-            <Button color='success' size='large' className='game-result__download-btn'>
-              Download statistics
-            </Button>
+        <div className='game-result__container'>
+          <h2 className='game-result__title'>{lobbyTitle || 'Results'}</h2>
+
+          <div className='game-result__statistics-file'>
+            <p className='game-result__small-text'>Click here to download json file with results</p>
+            <div className='game-result__download-file-card'>
+              <Button color='success' size='large' onClick={downloadBtnOnClick}>
+                Download statistics
+              </Button>
+            </div>
           </div>
+
           <div className='game-result__statistics'>
-            {gameResult?.map((issue: GameResultListProps, index: number) => (
-              <div className='game-result__statistic-wrapper'>
+            {gameResult.map((result, index) => (
+              <div className='game-result__data'>
                 <IssueCard
                   key={index.toString()}
-                  name={issue.issue?.name}
-                  priority={issue.issue?.priority}
-                  className='game-result__statistics-issue'
+                  name={result.issue.name}
+                  priority={result.issue.priority}
+                  className='game-result__issue'
                   color='dark'
                 />
 
-                <div className='game-result__cards-wrapper'>
-                  {issue.statistics?.map((element: StatisticsCardsProps, key: number) => (
-                    <div>
-                      <SpCardFront key={key.toString()} score={element.score} units={element.units} size='small' />
-                      <div className='game-result__statistics-percent'>{element.percent}</div>
+                <div className='game-result__values'>
+                  {result.votesPercentage.map((element, key) => (
+                    <div className='game-result__value-wrapper'>
+                      <SpCardFront
+                        key={key.toString()}
+                        score={element.value}
+                        units={scoreType}
+                        size='small'
+                        className='game-result__value-vote-card'
+                      />
+                      <div className='game-result__small-text'>{element.percentage}%</div>
                     </div>
                   ))}
                 </div>
               </div>
             ))}
           </div>
-        </div>
-        <div className='game-result__chat-wrapper'>
-          <Chat chatMessage={messages} className='game-result__chat' />
         </div>
       </div>
       <Footer />
